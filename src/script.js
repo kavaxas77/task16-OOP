0a675d8e41c67ff4f0ac3bcb7d57c6cd2c248c85
kavaxas77 <112971973+kavaxas77@users.noreply.github.com>
2022-10-28 19:04:26 +0400

    task 16 OOP

---
+++ src/script.js
@@ -0,0 +1,22 @@
+var movie = {
+    name : "Black clover movie",
+    language : ["Japanese" , "English"],
+    source : "Shonen Jump News",
+    expectation : ["medium" , "high"],
+    release : {
+        platform : ["Japanese cinema" , "Netflix"],
+    },
+    ratingImdb : [
+     {country: "japan", possible :8.0 },
+     {country: "usa" , possible :8.2},
+     {country: "germany" , possible :8.1}
+
+    ],
+    income : [
+        {"japan" :28000000},
+        {"world wide" :120000000},
+    ]
+};
+
+console.log(something[something][something])
+console.log(movie.income[japan])
